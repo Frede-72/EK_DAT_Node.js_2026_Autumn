8106dffa53e1d95e39e6101345762ab805764460
Frede-72 <frjo0005@stud.ek.dk>
2026-09-11 10:32:02 +0200

    handle non existing id

--- 00._Course_Material/01._Assignments/02._First_Server/src/app.js
+++ 00._Course_Material/01._Assignments/02._First_Server/src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const duck = { name:"Duck", id: 0 };
-const bird = { name:"Bird", id: 1 };
-const other_Animal = { name:"Other animal", id: 2 };
+const duck = { name:"Duck", id: 1 };
+const bird = { name:"Bird", id: 2 };
+const other_Animal = { name:"Other animal", id: 3 };
 
 
 
@@ -14,13 +14,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/animals', (req, res) => {
-    const animalNameList = []
-    animals.forEach(animal => animalNameList.push(animal.name));
-    res.send({ data: `All animals fall into one(or more) of these 3 categories ${ animalNameList }`});
+    
+    res.send({ data: animals});
 });
 
 app.get('/animals/:id', (req, res) => {
-    res.send({ data: `${animals[req.params.id].name} WOW this is(maybe) an animal. Try another animal` });
+    const provideId = Number(req.params.id);
+    const foundAnimal = animals.find((animal) => animal.id === provideId);
+
+    if (!foundAnimal) {
+        res.status(404).send({ data: `No animal found by id ${provideId}` });
+    } else {
+        res.send({ data: `${foundAnimal.name} WOW an animal. Try another animal` });
+    }
+
 });
+
+// 2xx OK
+// 3xx Redirect
+// 4xx Client-side error
+// 5xx Server-side error
 
 app.listen(8080);
